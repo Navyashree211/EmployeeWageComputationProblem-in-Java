a5155999652a36007c5505c953a17774a94f2a4d
navyas211@gmail.com <navyas211@gmail.com>
2021-09-16 18:27:26 +0530

    [NAVYA]ADDED: Employee is Present or Absent

--- EmpWageComputationProblem.java
+++ EmpWageComputationProblem.java
@@ -1,1 +1,22 @@
-#Welcome Employees........
+public class EmpWageComputationProblem
+ {
+
+	public static void main(String[] args)
+
+	{
+
+		final int FULL_TIME = 1;
+		int empType = (int) (Math.random() * 100) %2;
+		if (empType == FULL_TIME)
+		{
+			System.out.println("Employee is Present");
+		}
+
+		else
+		{
+			System.out.println("Employee is Absent");
+		}
+
+	}
+
+}
